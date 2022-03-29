9804d28831d3f342a89aa0faf4c7129a11c22632
Andrzej Kalinowski <jekrekk75@gmail.com>
2022-03-30 00:01:56 +0200

    html table

--- beclaw_bingo/bingo/views.py
+++ beclaw_bingo/bingo/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import saying
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the beclaw bingo index.")
+    # return HttpResponse("Hello, world. You're at the bęcław bingo index.")
+    # latest_question_list = saying.objects.order_by('-pub_date')[:5]
+    # context = {'latest_question_list': latest_question_list}
+    return render(request, 'index.html')
 
 def game(request):
     return HttpResponse("<h2>Beclaw Bingo!<h2>")
