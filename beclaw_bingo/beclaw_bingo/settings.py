4605d84d9cbcfbe0c5d99c3399ea7695315679d3
Andrzej Kalinowski <jekrekk75@gmail.com>
2022-03-29 23:16:08 +0200

    main structure

--- beclaw_bingo/beclaw_bingo/settings.py
+++ beclaw_bingo/beclaw_bingo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-m%)072rl*q$=l++deecyzmn$f!)g+q7ltre$=mj@@@$hei-gnj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "192.168.0.121", "127.0.0.1", "192.168.0.116"]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bingo.apps.BingoConfig'
 ]
 
 MIDDLEWARE = [
--- beclaw_bingo/bingo/apps.py
+++ beclaw_bingo/bingo/apps.py
@@ -1,6 +1,5 @@
 from django.apps import AppConfig
 
-
-class FrontendConfig(AppConfig):
+class BingoConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'frontend'
+    name = 'bingo'
